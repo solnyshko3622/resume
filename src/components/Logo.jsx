af553bc1342458855a20a3f844a0d742bb5297f3
Marya Yukhnina <m.yukhnina@g.nsu.ru>
2025-02-05 21:45:33 +0700

    change logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const Logo = ({ invert = false }) => {
     return (
         <div className="flex gap-3 items-center mt-2">
-            <img src="/logo.svg" alt="logo" className={`w-10 h-10 ${invert ? 'invert' : ''}`} />
+            <img src="/src/assets/logo.svg" alt="logo" className={`w-10 h-10 ${invert ? 'invert' : ''}`} />
             <h2 className={`text-lg font-bold ${invert ? 'text-white' : 'text-black'}`}>Yukhnina Marya</h2>
         </div>
     );
